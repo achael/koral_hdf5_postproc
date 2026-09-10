23d9b9608b56cc8d46349d392a949d97a47e7e83
Andrew Chael <achael@princeton.edu>
2026-09-10 15:53:59 +0200

    add simdata3D reader for raw KORAL dumps in koralh5postproc
    Pulls the header/grid/primitive reading and the derived-quantity calculation
    out of avgkoralhdf5s.phireduce_hdf5 and into a class, so that both the
    phi-reduction and a future 3D time-average can share it.
    
    The physics is unchanged: bsq and pgas come out bit-identical to the old
    inline path on ipole5000.h5, and u.u = -1, u.b = 0 to roundoff.
    
    Two things fixed along the way:
      - the bare "except:" around the read no longer swallows the error and then
        runs on undefined variables; the reader raises instead
      - has_photons is decided by looking for quants/nphot rather than by
        catching an exception

--- koralh5postproc.py
+++ koralh5postproc.py
@@ -11,6 +11,7 @@ from matplotlib.ticker import FormatStrFormatter
 from matplotlib import ticker
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from metricKS import *
+from koralopacities import *
 import h5py
 
 
@@ -30,6 +31,264 @@ SIGMA_RAD_CGS = 5.670367e-5
 A_RAD_CGS = 4*SIGMA_RAD_CGS/C_CGS
 TPFAC = KBOLTZ_CGS/(MP_CGS*C_CGS*C_CGS) # kelvin to dimensionless units for Tp
 TEFAC = KBOLTZ_CGS/(ME_CGS*C_CGS*C_CGS) # kelvin to dimensionless units for Te
+
+# upper-triangle stress tensor components we store
+TIDX = [(0,0),(0,1),(0,2),(0,3),(1,1),(1,2),(1,3),(2,2),(2,3),(3,3)]
+
+METRIC_DEFAULT = 'KS'
+KLEINNISHINA = False # we had KN turned off in KORAL runs for some reason...
+
+
+def _tostr(val):
+    """hdf5 string datasets come back as bytes on py3"""
+    if isinstance(val, bytes):
+        return val.decode('utf-8')
+    return str(val)
+
+
+class simdata3D(object):
+    """A raw KORAL ipole-format hdf5 dump plus the quantities derived from it.
+
+    The derived quantities are exposed lazily through field_names()/field() so a
+    caller can compute and consume them one at a time instead of materializing
+    every 3D array at once.  That is what lets the 3D time-average stream over
+    files without holding more than one running sum in memory.
+    """
+
+    def __init__(self, filename, metric=METRIC_DEFAULT, kn=KLEINNISHINA, verbose=False):
+        if metric not in ('KS','BL'):
+            raise Exception("metric must be 'KS' or 'BL'")
+        self.filename = filename
+        self.metric = metric
+        self.kn = kn
+        self.verbose = verbose
+
+        self._read()
+
+    @property
+    def shape(self):
+        return self.rho.shape
+
+    def _read(self):
+        """Read the header, the grid and the primitive variables out of the dump"""
+        if self.verbose: print('reading hdf5 ', self.filename, '....')
+
+        with h5py.File(self.filename, 'r') as fin:
+            self.time = float(fin['t'][()])
+
+            # get info from the header
+            head = fin['header']
+            self.spin = head['bhspin'][()].astype('f')
+            self.horiz = 1 + np.sqrt(1 - self.spin**2)
+            self.metric_run = _tostr(head['metric_run'][()])
+            self.metric_out = _tostr(head['metric_out'][()])
+            self.has_radiation = bool(head['has_radiation'][()])
+            self.has_electrons = bool(head['has_electrons'][()])
+
+            self.file_number = head['file_number'][()]
+            self.problem_number = head['problem_number'][()]
+            self.version = head['version'][()]
+
+            self.masssolar = head['units']['M_bh'][()]
+            self.mass_cgs = self.masssolar * MSUN_CGS
+            self.l_unit = head['units']['L_unit'][()] # length code2cgs = GM/c^2
+            self.u_unit = head['units']['U_unit'][()] # energy density code2cgs = c^8/G^3M^2
+            self.m_unit = head['units']['M_unit'][()] # mass density code2cgs = c^6/G^3M^2
+
+            if self.verbose:
+                print("M:%s, L:%s, U:%s, rho:%s"
+                      % (self.mass_cgs, self.l_unit, self.u_unit, self.m_unit))
+
+            if self.metric_out not in ('KS','BL'):
+                raise Exception("metric_out of KORAL h5 file must be KS or BL, got '%s'"
+                                % self.metric_out)
+
+            self.n1 = int(head['n1'][()])
+            self.n2 = int(head['n2'][()])
+            self.n3 = int(head['n3'][()])
+            self.gamma_head = head['gam'][()]
+
+            # get coordinates in OUTCOORDS (should be KS)
+            self.r  = fin['grid_out']['r'][:,:,:]
+            self.th = fin['grid_out']['th'][:,:,:]
+            self.ph = fin['grid_out']['ph'][:,:,:]
+
+            # get simulation primitive variables
+            quants = fin['quants']
+            self.rho = quants['rho'][:,:,:]
+            self.uint = quants['uint'][:,:,:]
+
+            self.u1_velr = quants['U1'][:,:,:]
+            self.u2_velr = quants['U2'][:,:,:]
+            self.u3_velr = quants['U3'][:,:,:]
+
+            self.B1 = quants['B1'][:,:,:]
+            self.B2 = quants['B2'][:,:,:]
+            self.B3 = quants['B3'][:,:,:]
+
+            if self.has_radiation:
+                self.erad = quants['erad'][:,:,:]
+                self.has_photons = 'nphot' in quants
+                if self.has_photons:
+                    self.nphot = quants['nphot'][:,:,:]
+                else:
+                    self.nphot = np.zeros(self.erad.shape)
+
+                self.ur1_velr = quants['F1'][:,:,:]
+                self.ur2_velr = quants['F2'][:,:,:]
+                self.ur3_velr = quants['F3'][:,:,:]
+            else:
+                self.has_photons = False
+
+            if self.has_electrons:
+                self.TeK = quants['te'][:,:,:]
+                self.TiK = quants['ti'][:,:,:]
+                self.gamma_adiab = quants['gammagas'][:,:,:]
+            else:
+                self.gamma_adiab = self.gamma_head
+
+    def set_derived_quantities(self):
+        """Metric, 4-velocities, b^mu, and the plasma parameters"""
+        spin, r, th = self.spin, self.r, self.th
+
+        # Metric
+        if self.metric == 'KS':
+            self.gcon = gconKS(spin, r, th)
+            lower = lowerKS
+            conv_vel = conv_vel_KS
+            trans_cov = trans_cov_bl2ks
+            invconv_vel = invconv_vel_KS
+        else:
+            self.gcon = gconBL(spin, r, th)
+            lower = lowerBL
+            conv_vel = conv_vel_BL
+            trans_cov = trans_cov_ks2bl
+            invconv_vel = invconv_vel_BL
+
+        # get 4-velocity
+        (u0,u1,u2,u3) = conv_vel(self.u1_velr, self.u2_velr, self.u3_velr, spin, r, th)
+        (u0_l,u1_l,u2_l,u3_l) = lower(u0, u1, u2, u3, spin, r, th)
+
+        if self.has_radiation:
+            (ur0,ur1,ur2,ur3) = conv_vel(self.ur1_velr, self.ur2_velr, self.ur3_velr, spin, r, th)
+            (ur0_l,ur1_l,ur2_l,ur3_l) = lower(ur0, ur1, ur2, ur3, spin, r, th)
+
+        # get magnetic field 4-vector and b^2
+        B1, B2, B3 = self.B1, self.B2, self.B3
+        b0 = u1_l*B1 + u2_l*B2 + u3_l*B3
+        b1 = (B1 + b0*u1)/u0
+        b2 = (B2 + b0*u2)/u0
+        b3 = (B3 + b0*u3)/u0
+
+        (b0_l,b1_l,b2_l,b3_l) = lower(b0, b1, b2, b3, spin, r, th)
+        bsq = b0*b0_l + b1*b1_l + b2*b2_l + b3*b3_l
+
+        # change coordinates
+        if self.metric_out != self.metric:
+            (u0,u1,u2,u3) = trans_cov(u0, u1, u2, u3, spin, r, th)
+            (u0_l,u1_l,u2_l,u3_l) = lower(u0, u1, u2, u3, spin, r, th)
+            self.u1_velr, self.u2_velr, self.u3_velr = invconv_vel(u0, u1, u2, u3, spin, r, th)
+
+            (b0,b1,b2,b3) = trans_cov(b0, b1, b2, b3, spin, r, th)
+            (b0_l,b1_l,b2_l,b3_l) = lower(b0, b1, b2, b3, spin, r, th)
+
+            if self.has_radiation:
+                (ur0,ur1,ur2,ur3) = trans_cov(ur0, ur1, ur2, ur3, spin, r, th)
+                (ur0_l,ur1_l,ur2_l,ur3_l) = lower(ur0, ur1, ur2, ur3, spin, r, th)
+                self.ur1_velr, self.ur2_velr, self.ur3_velr = invconv_vel(ur0, ur1, ur2, ur3, spin, r, th)
+
+        # (re) derive B-field 3 vector in case coordinates changed
+        self.B1 = b1*u0 - b0*u1
+        self.B2 = b2*u0 - b0*u2
+        self.B3 = b3*u0 - b0*u3
+
+        self.ucon = (u0,u1,u2,u3)
+        self.ucov = (u0_l,u1_l,u2_l,u3_l)
+        self.bcon = (b0,b1,b2,b3)
+        self.bcov = (b0_l,b1_l,b2_l,b3_l)
+        self.bsq = bsq
+        if self.has_radiation:
+            self.urcon = (ur0,ur1,ur2,ur3)
+            self.urcov = (ur0_l,ur1_l,ur2_l,ur3_l)
+
+        # plasma parameters
+        self.pgas = (self.gamma_adiab - 1.)*self.uint           # pressure
+        self.w = (self.rho + self.pgas + self.uint)             # enthalpy
+        self.TgasK = (self.pgas / self.rho) * (MU_GAS / TPFAC)  # gas temperature in Kelvin
+
+        # radiation derived quantities
+        if self.has_radiation:
+            self._set_radiation_quantities()
+
+        # electron derived quantities
+        if self.has_electrons:
+            self._set_electron_quantities()
+
+        return
+
+    def _set_electron_quantities(self):
+        """electron/ion pressures and the heating functions"""
+        self.pi = (self.rho/MU_I)*TPFAC*self.TiK  # ion pressure, code units
+        self.pe = (self.rho/MU_E)*TPFAC*self.TeK  # electron pressure, code units
+
+        # heating functions (unitless)
+        self.deltaeK = deltaeKawazura(2*self.pi/self.bsq, self.TeK/self.TiK)
+        self.deltaeZ = deltaeZhdankin(TEFAC*self.TeK, TPFAC*self.TiK/MU_I, MRATIO)
+
+        return
+
+    def _set_radiation_quantities(self):
+        """fluid frame radiation quantities and the opacities, all in code units"""
+        erad = self.erad
+
+        # fluid frame radiation energy density
+        erad_hat = np.zeros(erad.shape)
+        for i in range(4):
+            for j in range(4):
+                erad_hat += ((4./3.)*erad*self.urcon[i]*self.urcon[j]
+                             + (1./3.)*erad*self.gcon[i][j])*self.ucov[i]*self.ucov[j]
+        self.erad_hat = erad_hat
+
+        # fluid frame photon number
+        if self.has_photons:
+            nphot_hat = np.zeros(self.nphot.shape)
+            for i in range(4):
+                nphot_hat += -self.nphot*self.urcon[i]*self.ucov[i]
+            self.nphot_hat = nphot_hat
+
+            # radiation temperature in the fluid frame
+            self.TradK = erad_hat/(2.7012*nphot_hat) * (self.mass_cgs*C_CGS*C_CGS / KBOLTZ_CGS) # in Kelvin TODO check
+        else:
+            self.TradK = ((erad_hat*self.u_unit / A_RAD_CGS))**(0.25) # in Kelvin
+            self.nphot_hat = ((A_RAD_CGS * (self.TradK**3) / (2.70118*KBOLTZ_CGS))
+                              * (self.mass_cgs*C_CGS*C_CGS / self.u_unit))
+
+        # opacities
+        # u_unit = c^8/G^3 M^2 is the transformation for energy density from code --> cgs
+        rhocgs = self.rho * (self.u_unit / (C_CGS*C_CGS))
+        Bmagcgs = np.sqrt(self.bsq) * np.sqrt(4*np.pi*self.u_unit)
+
+        if not self.has_electrons:
+            self.TeK = self.TgasK
+
+        # opacities (compute in cgs from koralopacities.py and return to code units)
+        # TODO include scattering?
+        (synemisopac, synabsorbopac) = synopac(rhocgs, self.TeK, self.TradK, Bmagcgs)
+        (ffemisopac, ffabsorbopac) = ffopac(rhocgs, self.TeK, self.TradK)
+        comptabsorbopac = comptopac(rhocgs, self.TeK, self.TradK, kn=self.kn)
+
+        # return to code units
+        self.synemisopac = synemisopac * self.l_unit
+        self.synabsorbopac = synabsorbopac * self.l_unit
+        self.ffemisopac = ffemisopac * self.l_unit
+        self.ffabsorbopac = ffabsorbopac * self.l_unit
+        self.comptabsorbopac = comptabsorbopac * self.l_unit
+
+        # blackbody energy density (code units)
+        self.BBenergy = A_RAD_CGS*(self.TeK**4) / self.u_unit
+
+        return
+
 
 class simdata2D(object):
     def __init__(self, filename, metric, r, th):
